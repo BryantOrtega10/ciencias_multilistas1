dfb37f355e454922a02d857ebc0b6fd93b8d9a12
BryantOrtega10 <bryant.ortega1010@gmail.com>
2021-08-28 23:20:45 -0500

    Eliminar completo

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ int main(int argc, char** argv) {
 
 	setlocale(LC_ALL, "spanish");
 	Multilista ml;
-	
-	//0 => "Ing. El�ctrica", 1 => "Ing. Electr�nica",2 => "Ing. de Sistemas", 3 => "Ing. Industrial"
-	//0 => "Danza", 1=> "Nataci�n", 2 => "B�squet", 3 => "B�isbol"
+
+	//0 => "Ing. Eléctrica", 1 => "Ing. Electrónica",2 => "Ing. de Sistemas", 3 => "Ing. Industrial"
+	//0 => "Danza", 1=> "Natación", 2 => "Básquet", 3 => "Béisbol"
 
 	ml.insertar("Raquel", 54, 1, 1);
 	ml.insertar("Jaime", 38, 1, 1);
@@ -22,17 +22,36 @@ int main(int argc, char** argv) {
 	ml.insertar("Italia", 31, 1, 3);
 	ml.insertar("Mario", 18, 1, 3);
 	ml.insertar("Pilar", 22, 1, 3);
-	ml.insertar("Xiomara", 101, 1, 3);
+	ml.insertar("Xiomara", 101, 2, 3);
 
+	cout << "LISTA SISTEMAS:" << endl;
+	ml.listar(2);//Ing. de Sistemas
+	cout << endl;
+	cout << "LISTA BEISBOL:" << endl;
 	ml.listar(7);//Beisbol
 	cout << endl;
+	cout << "LISTA NOMBRE ASC:" << endl;
 	ml.listar(8);//Nombre Asc
 	cout << endl;
+	cout << "LISTA EDAD:" << endl;
 	ml.listar(9);//Edad Desc
 	cout << endl;
-
+	cout << "BUSCAR:" << endl;
 	ml.buscar_nombre("Xiomara");
-
+	cout << "ELIMINAR:" << endl;
+	ml.eliminar("Xiomara");
+	cout << "LISTA SISTEMAS:" << endl;
+	ml.listar(2);//Ing. de Sistemas
+	cout << endl;
+	cout << "LISTA BEISBOL:" << endl;
+	ml.listar(7);//Beisbol
+	cout << endl;
+	cout << "LISTA NOMBRE ASC:" << endl;
+	ml.listar(8);//Nombre Asc
+	cout << endl;
+	cout << "LISTA EDAD:" << endl;
+	ml.listar(9);//Edad Desc
+	cout << endl;
 	//ml.eliminar()
 
 	return 0;
--- multilista.h
+++ multilista.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <iomanip> 
 #ifndef MULTILISTA_H     
 #define MULTILISTA_H 
 
@@ -9,7 +10,7 @@ class Multilista {
 private:
 	int tam;
 	nodo* lista_cabezas[10];
-	string listaCarreras[4] = {"Ing. Elóctrica", "Ing. Electrónica","Ing. de Sistemas", "Ing. Industrial"};
+	string listaCarreras[4] = { "Ing. Elóctrica", "Ing. Electrónica","Ing. de Sistemas", "Ing. Industrial" };
 	string listaActividades[4] = { "Danza", "Natación","Básquet", "Béisbol" };
 
 
@@ -28,7 +29,7 @@ public:
 	void asignar_pos_edad(nodo* nueva_persona);
 	void asignar_pos_carrera(nodo* nueva_persona, int i_carrera);
 	void asignar_pos_actividad(nodo* nueva_persona, int i_actividad);
-	
+
 	void listar(int i_cab);
 	void buscar_nombre(string nombre);
 
@@ -60,11 +61,11 @@ void Multilista::asignar_pos_nombre(nodo* nueva_persona) {
 		nodoAnterior = aux;
 		aux = aux->sig[0];
 	}
-	if (aux->nombre < nueva_persona->nombre){
+	if (aux->nombre < nueva_persona->nombre) {
 		nueva_persona->sig[0] = aux->sig[0];
 		nodoAnterior = aux;
 	}
-	else{
+	else {
 		nueva_persona->sig[0] = aux;
 	}
 
@@ -73,7 +74,7 @@ void Multilista::asignar_pos_nombre(nodo* nueva_persona) {
 	}
 	else {
 		lista_cabezas[8] = nueva_persona;
-	}	
+	}
 }
 
 void Multilista::asignar_pos_edad(nodo* nueva_persona) {
@@ -89,14 +90,14 @@ void Multilista::asignar_pos_edad(nodo* nueva_persona) {
 		aux = aux->sig[1];
 	}
 
-	if (aux->edad > nueva_persona->edad){
+	if (aux->edad > nueva_persona->edad) {
 		nueva_persona->sig[1] = aux->sig[1];
 		nodoAnterior = aux;
 	}
 	else {
 		nueva_persona->sig[1] = aux;
 	}
-	
+
 	if (nodoAnterior != NULL) {
 		nodoAnterior->sig[1] = nueva_persona;
 	}
@@ -110,7 +111,7 @@ void Multilista::asignar_pos_carrera(nodo* nueva_persona, int i_carrera) {
 	if (lista_cabezas[i_carrera] == NULL) {
 		lista_cabezas[i_carrera] = nueva_persona;
 	}
-	else{
+	else {
 		nodo* aux = lista_cabezas[i_carrera];
 		while (aux->sig[2] != NULL) {
 			aux = aux->sig[2];
@@ -130,10 +131,10 @@ void Multilista::buscar_nombre(string nombre) {
 		aux = aux->sig[0];
 	}
 	if (aux->nombre == nombre) {
-		cout << aux->nombre <<setw(15)<<"\t" << aux->edad << "\t" << *aux->carrera << "\t" << *aux->actividad << endl;
+		cout << aux->nombre << setw(15) << "\t" << aux->edad << "\t" << *aux->carrera << "\t" << *aux->actividad << endl;
 		return;
 	}
-	
+
 	cout << "Nombre no encontrado" << endl;
 	return;
 }
@@ -151,9 +152,73 @@ void Multilista::listar(int i_cab) {
 
 	nodo* aux = lista_cabezas[i_cab];
 	while (aux != NULL) {
-		cout << aux->nombre <<setw(15)<<"\t" << aux->edad << "\t" << *aux->carrera << "\t" << *aux->actividad << endl;
+		cout << aux->nombre << setw(15) << "\t" << aux->edad << "\t" << *aux->carrera << "\t" << *aux->actividad << endl;
 		aux = aux->sig[i_sig];
 	}
+}
+
+bool Multilista::eliminar(string nombre) {
+	nodo* aux[4];
+	aux[0] = lista_cabezas[8];
+	nodo* nodoAnterior[4] = {NULL, NULL, NULL, NULL};
+	if (aux == NULL) {
+		return false;
+	}
+	while (aux[0]->nombre != nombre && aux[0]->sig[0] != NULL) {
+		nodoAnterior[0] = aux[0];
+		aux[0] = aux[0]->sig[0];
+	}
+	if (aux[0]->nombre == nombre) {
+		//Buscar otros nodos anteriores 
+		aux[1] = lista_cabezas[9];
+		int i_carrera, i_actividad;
+
+		if (aux[0]->carrera == &listaCarreras[0]) i_carrera = 0;
+		else if (aux[0]->carrera == &listaCarreras[1]) i_carrera = 1;
+		else if (aux[0]->carrera == &listaCarreras[2]) i_carrera = 2;
+		else if (aux[0]->carrera == &listaCarreras[3]) i_carrera = 3;
+
+		aux[2] = lista_cabezas[i_carrera];
+
+		if (aux[0]->actividad == &listaActividades[0]) i_actividad = 4;
+		else if (aux[0]->actividad == &listaActividades[1]) i_actividad = 5;
+		else if (aux[0]->actividad == &listaActividades[2]) i_actividad = 6;
+		else if (aux[0]->actividad == &listaActividades[3]) i_actividad = 7;
+		
+		aux[3] = lista_cabezas[i_actividad];
+
+		while (aux[1]->nombre != nombre && aux[1]->sig[1] != NULL) {
+			nodoAnterior[1] = aux[1];
+			aux[1] = aux[1]->sig[1];
+		}
+		while (aux[2]->nombre != nombre && aux[2]->sig[2] != NULL) {
+			nodoAnterior[2] = aux[2];
+			aux[2] = aux[2]->sig[2];
+		}
+		while (aux[3]->nombre != nombre && aux[3]->sig[3] != NULL) {
+			nodoAnterior[3] = aux[3];
+			aux[3] = aux[3]->sig[3];
+		}
+
+
+		if (nodoAnterior[0] != NULL) nodoAnterior[0]->sig[0] = aux[0]->sig[0];
+		else lista_cabezas[8] = aux[0]->sig[0];
+
+		if (nodoAnterior[1] != NULL) nodoAnterior[1]->sig[1] = aux[1]->sig[1];
+		else lista_cabezas[9] = aux[1]->sig[1];
+
+		if (nodoAnterior[2] != NULL) nodoAnterior[2]->sig[2] = aux[2]->sig[2];
+		else lista_cabezas[i_carrera] = aux[2]->sig[2];
+
+		if (nodoAnterior[3] != NULL) nodoAnterior[3]->sig[3] = aux[3]->sig[3];
+		else lista_cabezas[i_actividad] = aux[3]->sig[3];
+
+		delete aux[0];
+		tam--;
+		return true;
+	}
+
+	return false;
 }
 
 #endif
